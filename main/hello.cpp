580d8f0e2102fc24be52bdb3d21bb75901be2ec8
AGeethikachowdary <113278357+AGeethikachowdary@users.noreply.github.com>
2024-03-05 18:21:18 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "My SRN is PES1UG21CS739\n";
+  cout << "My Name is Geethika\n";
   return 0;
 }
